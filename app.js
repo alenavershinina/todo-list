b86029f0dfb6bdff9516d4aca99aa72bc4bc348c
Alena Vershinina <azudkova@gmail.com>
2020-12-26 00:09:27 +0100

    GET / modification

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello");
+  var today = new Date();
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.send("Yay! It's weekend!");
+  } else {
+    res.send("Go to work")
+  }
 });
 
 app.listen(3000, function() {
