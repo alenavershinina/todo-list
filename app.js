0df602c05359594cd528a88d5a142589019eb7f4
Alena Vershinina <azudkova@gmail.com>
2020-12-27 13:47:37 +0100

    Added an unordered list and opportunity to add new items in the list

--- app.js
+++ app.js
@@ -3,13 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.get("/", function(req, res) {
+
   var today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay! It's weekend!");
-  } else {
-    res.send("Go to work")
-  }
+  var options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
