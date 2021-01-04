b85408b461209c9457034396f230382436468d1d
Alena Vershinina <azudkova@gmail.com>
2021-01-04 01:09:21 +0100

    added mongoDB to the project, checkbox now completely deletes items

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,14 +12,48 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to the ToDo App"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name: "<== Hit this to delete the item"
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function(req, res) {
   let day = date();
-  res.render("list", {
-    listName: day,
-    newListItems: items
+  Item.find({}, function(err, items) {
+    if (items.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully added default items to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listName: day,
+        newListItems: items
+      });
+    }
   });
 });
 
@@ -39,9 +74,22 @@ app.post("/", function(req, res) {
     workItems.push(item);
     res.redirect("/work");
   } else {
-    items.push(item);
+    Item({
+      name: item
+    }).save();
     res.redirect("/");
   }
+});
+
+app.post("/delete", function(req, res) {
+  Item.findByIdAndRemove(req.body.checkbox, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully removed item from database");
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function() {
